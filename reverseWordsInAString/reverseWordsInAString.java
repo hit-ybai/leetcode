dee1f8e37dd05fbcd72e4415339079e68563ac20
ybai <ybai@freewheel.tv>
2014-10-30 17:52:54 +0800

    Reverse Words in a String with Java

---
+++ reverseWordsInAString/reverseWordsInAString.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class Solution {
+    public static String reverseWords(String s) {
+    	String[] temp = s.split(" ");
+    	int len = temp.length;
+
+		Stack <String> stack = new Stack();
+		for (int i = 0; i < len; i++)
+		{
+			if (!temp[i].equals("")) stack.push(temp[i]);
+		}
+
+		String ans = "";
+        while (false == stack.isEmpty())
+        {
+        	ans += stack.pop();
+        	if (!stack.isEmpty())ans += ' ';
+        }
+
+        return ans;
+    }
+}
